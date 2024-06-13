a04fcbc85bb56ca0d1e001104af31184d495a7cd
fgarrasino <fgarrasino@udesa.edu.ar>
2024-06-13 14:31:38 -0300

    agregue category.js y producto.js

---
+++ js/category.js
@@ -0,0 +1,9 @@
+fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(json=>console.log(json))
+
+.then (function(data){
+    console.log(data);
+    let section= document.querySelector (".products")
+    let product
+})
