8d04f1ba03bc8a3d380aea9fe90a1cf18d3ec9af
fgarrasino <fgarrasino@udesa.edu.ar>
2024-06-23 15:48:25 -0300

    estoy tratando de hacer el logout

--- js/logout.js
+++ js/logout.js
@@ -1,20 +1,22 @@
 
 
 let logoutLink = document.querySelector('#logout-link');
+let welcomeMessage = document.querySelector('#welcomeMessage');
+let loginLink = document.querySelector('#login-link');
+let registerLink = document.querySelector('#register-link');
 
 if (logoutLink) {
     logoutLink.addEventListener('click', function(event) {
         event.preventDefault();
-        
-        // Para sacar la informacion del local storage
+
         localStorage.removeItem('userEmail');
-        
-        // Volver a mostrar login y register links
-        let loginLink = document.querySelector('#login-link');
-        let registerLink = document.querySelector('#register-link');
 
-        if (loginLink) loginLink.innerText = 'Log in';
-        if (registerLink) registerLink.innerText = 'Register';
+        welcomeMessage.style.display = 'none';
+        logoutLink.style.display = 'none';
 
+        if (loginLink) loginLink.style.display = 'block';
+        if (registerLink) registerLink.style.display = 'block';
+
+        document.location.href = "./index.html";
     });
 }
